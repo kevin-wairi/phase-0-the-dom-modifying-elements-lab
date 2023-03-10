2e84a969dd641f7b53c472c851844afdf27c6ff2
kevinwairi <kevinwairi@gmail.com>
2023-03-10 15:14:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your code here!
+const main = document.querySelector('main');
+main.remove();
+
+// const addDiv = document.createElement('div');
+// addDiv.innerHTML = "Contents inside a div"
+// document.body.append(addDiv);
+
+const newHeader = document.createElement('H1');
+// addTag.className = 'newHeader';
+newHeader.setAttribute("id","victory");
+newHeader.innerHTML = "YOUR-NAME is the champion";
+document.body.append(newHeader);
+
+
+ //const giveId = document.getElementsByTagName('h1');
+// template.innerHTML = 'content inside the div';
+// const newHeader = document.getElementsByTagName('H1');
+// addClass.classList.add("newHeader");    
+
+
+
